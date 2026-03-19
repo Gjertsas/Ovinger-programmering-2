b061fa42012e62e6d0bd21f1829e840fcdf15539
Sivertgj <Sivertgj@stud.ntnu.no>
2026-03-19 12:11:57 +0100

    initial commit handofcards

---
+++ cardgame/src/main/java/no/ntnu/cardgame/HandOfCards.java
@@ -0,0 +1,12 @@
+package no.ntnu.cardgame;
+ 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * Representerer en hånd med kort utdelt til en spiller.
+ * Streams-baserte metoder for å analysere hånden.
+ */
+public class HandOfCards {
+}
