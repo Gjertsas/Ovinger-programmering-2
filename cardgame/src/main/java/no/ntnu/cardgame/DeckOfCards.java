a9c9fb613f7251fa89a70544bdc9643d2959efe7
Sivertgj <Sivertgj@stud.ntnu.no>
2026-03-19 12:00:46 +0100

    Added method for constructing a full deck of cards

--- cardgame/src/main/java/no/ntnu/cardgame/DeckOfCards.java
+++ cardgame/src/main/java/no/ntnu/cardgame/DeckOfCards.java
@@ -13,4 +13,19 @@ public class DeckOfCards {
   private final char[] suits = {'S', 'H', 'D', 'C'};
   private final List<PlayingCard> deck;
   private final Random random;
+
+  /**
+   * Lager full stokk med 52 kort, 4 sorter og 13 verdier
+   */
+  public DeckOfCards() {
+    deck = new ArrayList<>();
+    random = new Random();
+
+    for (char suit : suits) {
+      for (int facevalue = 1; facevalue <= 13; facevalue++) {
+        deck.add(new PlayingCard(0, facevalue));
+      }
+    }
+  }
+  
 }
