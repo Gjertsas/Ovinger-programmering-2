199b481a04e3de0d9d8b58583079f4c4d5745abc
Sivertgj <Sivertgj@stud.ntnu.no>
2026-03-20 10:04:26 +0100

    DeckOfCards-tester

--- cardgame/src/test/java/no/ntnu/cardgame/CardGameTest.java
+++ cardgame/src/test/java/no/ntnu/cardgame/CardGameTest.java
@@ -20,3 +20,28 @@ class CardGameTest {
       deck = new DeckOfCards();
   }
 
+  // DeckOfCards
+ @Test
+  void dealHand_returnsCorrectNumberOfCards() {
+    HandOfCards hand = deck.dealHand(5);
+    assertEquals(5, hand.getCards().size());
+  }
+
+  @Test
+  void dealHand_returnsUniqueCards() {
+    HandOfCards hand = deck.dealHand(52);
+    long uniqueCount = hand.getCards().stream().distinct().count();
+    assertEquals(52, uniqueCount);
+  }
+
+    @Test
+    void dealHand_throwsOnInvalidArgument_zero() {
+        assertThrows(IllegalArgumentException.class, () -> deck.dealHand(0));
+    }
+
+    @Test
+    void dealHand_throwsOnInvalidArgument_tooMany() {
+        assertThrows(IllegalArgumentException.class, () -> deck.dealHand(53));
+    }
+
+}
